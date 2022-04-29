61afc58de49bb3e0ec8773b96ad52f79937b6e6d
Dhruv Keyal <dkeyal@umass.edu>
2022-04-28 21:23:30 -0400

    added trend graph - not showing up

--- main.js
+++ main.js
@@ -168,7 +168,52 @@ function buildBarChart(a1, a2, vis_div, data_url) {
 }
 
 //buildTrendGraph => None
-function buildTrendGraph() {
+function buildTrendGraph(a1, a2, vis_div, data_url) {
+    // set the dimensions and margins of the graph
+    const [margin, width, height] = getDimensions();
+
+    // append the svg object to the body of the page
+    var svg = d3.select(vis_div)
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //Read the data
+    d3.csv(data_url, 
+
+        // format variables
+        function(d){
+            return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+          },
+        function(data) {
+            // Add X axis --> it is a date format
+            var x = d3.scaleTime()
+                .domain(d3.extent(data, function(d) { return d.date; }))
+                .range([ 0, width ]);
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return +d.value; })])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Add the line
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+            .x(function(d) { return x(d.date) })
+            .y(function(d) { return y(d.value) })
+            )
+    })
 
 }
 
