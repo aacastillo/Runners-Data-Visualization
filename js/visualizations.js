6359d1aedff9995ff966c3f82e69c95cf8528190
Dhruv Keyal <dkeyal@umass.edu>
2022-05-01 20:50:50 -0400

    added tooltip for tend - not working properly

--- js/visualizations.js
+++ js/visualizations.js
@@ -117,6 +117,27 @@ function buildTrendGraph(a1, vis_div, data_url) {
                 .text(a1);
             svg.append("g")
                 .call(d3.axisLeft(y));
+            
+            // This allows to find the closest X index of the mouse:
+            var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+            // Create the circle that travels along the curve of chart
+            var focus = svg
+                .append('g')
+                .append('circle')
+                .style("fill", "none")
+                .attr("stroke", "black")
+                .attr('r', 8.5)
+                .style("opacity", 0)
+
+            // Create the text that travels along the curve of chart
+            var focusText = svg
+                .append('g')
+                .append('text')
+                .style("opacity", 0)
+                .attr("text-anchor", "left")
+                .attr("alignment-baseline", "middle")
+
 
             // Add the line
             svg.append("path")
@@ -128,6 +149,42 @@ function buildTrendGraph(a1, vis_div, data_url) {
                 .x(function(d) { return x(d.date) })
                 .y(function(d) {return y(d.value) })
                 )
+            
+            // Create a rect on top of the svg area: this rectangle recovers mouse position
+            svg
+            .append('rect')
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .attr('width', width)
+            .attr('height', height)
+            .on('mouseover', mouseover)
+            .on('mousemove', mousemove)
+            .on('mouseout', mouseout);
+
+
+            // What happens when the mouse move -> show the annotations at the right positions.
+            function mouseover() {
+            focus.style("opacity", 1)
+            focusText.style("opacity",1)
+            }
+
+            function mousemove() {
+            // recover coordinate we need
+            var x0 = x.invert(d3.mouse(this)[0]);
+            var i = bisect(data, x0, 1);
+            selectedData = data[i]
+            focus
+                .attr("cx", x(selectedData.date))
+                .attr("cy", y(selectedData.value))
+            focusText
+                .html("x:" + selectedData.date + "  -  " + "y:" + selectedData.value)
+                .attr("x", x(selectedData.date)+15)
+                .attr("y", y(selectedData.value))
+            }
+            function mouseout() {
+            focus.style("opacity", 0)
+            focusText.style("opacity", 0)
+            }
         }
     )
 }
